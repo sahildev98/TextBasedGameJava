9c03cd7ba1180976a217be0553089dc0929156a7
Sahil Ahmed <sahil.98@hotmail.com>
2021-04-01 20:35:41 +0100

    Instance variables added + Constructor

--- TextBasedGame/src/main/java/com/mycompany/textbasedgame/Robot.java
+++ TextBasedGame/src/main/java/com/mycompany/textbasedgame/Robot.java
@@ -10,5 +10,13 @@ package com.mycompany.textbasedgame;
  * @author Sahil
  */
 public class Robot {
-    
+    private String name;
+    private String gender;
+    private int health; 
+
+    public Robot(String name, String gender, int health) {
+        this.name = name;
+        this.gender = gender;
+        this.health = health;
+    }
 }
