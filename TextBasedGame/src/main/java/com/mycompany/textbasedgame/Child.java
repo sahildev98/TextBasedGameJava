b640d826a56196dc8574f4809e6473cb8f76f484
Sahil Ahmed <sahil.98@hotmail.com>
2021-04-07 00:14:48 +0100

    Updated attack method.

--- TextBasedGame/src/main/java/com/mycompany/textbasedgame/Child.java
+++ TextBasedGame/src/main/java/com/mycompany/textbasedgame/Child.java
@@ -5,19 +5,40 @@
  */
 package com.mycompany.textbasedgame;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Sahil
  */
 public class Child extends Person {
-    private String attack;
-    public Child(String name, int age, String gender, int health) {
-        super(name, age, gender, health);
+    static Scanner in = new Scanner(System.in);
+    public Child(String forename,String surname,String nickname, int age, String gender, int health) {
+        super(forename, surname, nickname, age, gender, health);
     }
     
+    @Override
     public void attack(){
+        System.out.println("Do you want to attack?");
+        String attack = in.nextLine();
+        if(attack.equals("Yes")|| attack.equals("yes")){
         String attack_Msg = "Attack!!!";
         System.out.println(attack_Msg);
         System.out.println(" is attacking ");
+        } else {
+            System.out.println(" is not attacking");
+        }
+        
+    }
+
+    @Override
+    public void move() {
+        System.out.println("Do you want to move to another location?" + "\n"  + "Enter yes or no.");
+        String move = in.nextLine();
+        if(move.equals("Yes") || move.equals("Yes")){
+            System.out.println(" is moving ");
+        } else {
+            System.out.println(" is not moving.");
+        }
     }
 }
