a8460fba2bc89e4ad83bbff166a1909ea1e87046
Sahil Ahmed <sahil.98@hotmail.com>
2021-04-07 18:12:03 +0100

    Updated item list. Updated control structure of the game with new if statements for case front room. Added another question to ask user whether to move or not.

--- TextBasedGame/src/main/java/com/mycompany/textbasedgame/TestGame.java
+++ TextBasedGame/src/main/java/com/mycompany/textbasedgame/TestGame.java
@@ -18,7 +18,7 @@ public class TestGame {
     public static void main(String[] args) {
         Child dennis = new Child("Dennis", "Menance","The Menance", 10, "Male", 30);
         Adult sandra =  new Adult ("Sandra", "Menance", "Dennis's Mum", 38, "Female", 30, "parent");
-        Adult dennisSnr = new Adult("Dennis","Menance", "Dennis's Dad", 42, "Male", 20, "Stationary Worksman");
+        Adult dennisSnr = new Adult("Dennis Snr","Menance", "Dennis's Dad", 42, "Male", 20, "Stationary Worksman");
         Child bea =  new Child ("Beatrice","Menance", "Bea", 3, "Female", 20);
         Adult granny = new Adult("Granny","Menance", "Granny", 80, "Female", 20, "GrandParent");
         Child walter = new Child ("Walter","Brown", "The softy", 12, "Male", 20);
@@ -50,17 +50,18 @@ public class TestGame {
         items.add("Hats");
         items.add("Masks");
         items.add("Fake beard");
+        items.add("TV");
         
         System.out.println("Welcome to Dennis The meneance Text Editior Game!");
         System.out.println("Do you want to start the game?"  + "\n" +  "Enter yes or no.");
         String start = in.nextLine();
-        if(start.equals ("Yes") || start.equals ("yes")){
+        if(start.equals("Yes") || start.equals("yes") || start.equals("YES")){
             System.out.println("You have entered the Menance World!");
         } else{
-            System.out.println("Game over");
+            System.out.println("Game over!" + "\n" + "Start again");
             System.exit(0);
         }
-        System.out.println("Where you want to enter first with " + dennis.getForename()+ "?" + "\n" + "Enter any of the following locations: ");
+        System.out.println("Where you want to enter first with " + dennis.getForename() + " and " + gnasher.getName() + "?" + "\n" + "Enter any of the following locations: ");
         for(String i: locations){
             System.out.println(i);
         }
@@ -71,23 +72,53 @@ public class TestGame {
             switch (enter) {
                 case "The front room":
                 case "the front room":
-                    System.out.println(dennis.getForename() + " is heading to the " + locations.get(0));
+                    System.out.println(dennis.getForename() + " and " + gnasher.getName() + " are in " + locations.get(0));
+                    System.out.println("Do you want to prank anyone?" + "Enter yes or no.");
+                    String askPrank = in.nextLine();
+                    if (askPrank.equals("yes") || askPrank.equals("Yes") || askPrank.equals("yes")) {
+                        System.out.println("Enter what prank you want to do" + "\n" + "BEWARE YOU COULD LOSE THE GAME!");
+                        String prank = in.nextLine();
+                        System.out.println("Who do you want " + prank + " do this to?" + "\n" + "Enter any of the following names" + "\n"
+                                + sandra.getForename() + "\n" + dennisSnr.getForename());
+                        String prankWho = in.nextLine();
+                        if (prankWho.equals("sandra") || prankWho.equals("Sandra") || prankWho.equals("SANDRA")) {
+                            System.out.println(dennis.getForename() + " " + prank + " " + sandra.getNickname());
+                            sandra.reduceHealth();
+                            sandra.issueBan();
+                        } else if (prankWho.equals("dennis snr") || prankWho.equals("dennis Snr") || prankWho.equals("Dennis Snr") || prankWho.equals("DENNIS SNR")) {
+                            System.out.println(dennis.getForename() + " " + prank + " " + dennis.getNickname());
+                            dennisSnr.reduceHealth();
+                            dennisSnr.issueBan();
+                        } else {
+                            System.out.println(dennis.getForename() + " does not want to prank");
+                        }
+                        
+                        boolean tv = false;
+                        System.out.println("Do you want to watch "  + items.get(7) + "?" + "\n" + " Enter yes or no");
+                        String watchTV = in.nextLine();
+                        if(watchTV.equals("yes") || watchTV.equals("Yes") || watchTV.equals("YES")){
+                            tv = true;
+                            System.out.println(dennis.getForename() + " is watching football on " + items.get(7));
+                        } else {
+                            System.out.println(dennis.getForename() +  " does not want to watch "  + items.get(7));
+                        }       
+                    }
                     break;
                 case "The treehouse":
                 case "the treehouse":
-                    System.out.println(dennis.getForename() + " is heading to the " + locations.get(1));
+                    System.out.println(dennis.getForename() + " are in " + locations.get(1));
                     break;
                 case "The park":
                 case "the park":
-                    System.out.println(dennis.getForename() + " is heading to the " + locations.get(2));
+                    System.out.println(dennis.getForename() + " are in " + locations.get(2));
                     break;
                 case "MI 13 Secret Service headquarters":
                 case "mi 13 secret service headquarters":
-                    System.out.println(dennis.getForename() + " is heading to the " + locations.get(3));
+                    System.out.println(dennis.getForename() + " are in the " + locations.get(3));
                     break;
                 case "Bash street school":
                 case "bash street school":
-                    System.out.println(dennis.getForename() + " is heading to the " + locations.get(4));
+                    System.out.println(dennis.getForename() + " are in " + locations.get(4));
                     break;
                 default:
                     System.out.println("Enter Location again.");
@@ -97,6 +128,20 @@ public class TestGame {
             }
          }
          while(needAnswer);
+         
+         
+         
+         System.out.println("Do you want to move to another location?" + "\n" + "Enter Yes or no");
+         enter = in.nextLine();
+         if(enter.equals("yes") || enter.equals("Yes") || enter.equals("YES")){
+            System.out.println("Enter location");
+             enter = in.nextLine();
+             System.out.println(dennis.move() + "with " + gnasher.getName() + " to " + enter);
+             
+         }else {
+             System.out.println(dennis.getForename()+ " is not moving.");
+         }
+         
         } 
          
     }
